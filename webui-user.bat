4244255748b22cc1982de42457cf69d7f0494e99
Allan <allanluk.ca@gmail.com>
2023-01-31 07:03:19 -0800

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -1,8 +1,8 @@
 @echo off
 
-set PYTHON=
+set PYTHON="C:\Users\Allan\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
-
+set COMMANDLINE_ARGS=--medvram --opt-split-attention --xformers
+git pull
 call webui.bat
