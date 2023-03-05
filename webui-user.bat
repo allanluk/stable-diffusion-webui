f7bfbd5f91ff53f48dccd1cec1c3c120b7644495
Allan <allanluk.ca@gmail.com>
2023-03-05 09:06:17 -0800

    Personal Changes

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,6 @@
 set PYTHON="C:\Users\Allan\AppData\Local\Programs\Python\Python310\python.exe"
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=--medvram --opt-split-attention --xformers
+set COMMANDLINE_ARGS=--medvram --opt-split-attention --xformers --api
 git pull
 call webui.bat
